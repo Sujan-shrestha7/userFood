e23e2cea93a292ac53ec06d6b71d5e3f27450d02
Sujanshrestha <sujanstha5732@gmail.com>
2025-02-06 14:38:36 +0545

    navbar

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Router from './Router'
+
+
+const App = () => {
+  return (
+    <div>
+      {/* <Home /> */}
+      {/* <LineChartComponent/> */}
+      {/* <Sidebar/> */}
+      <Router/>
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Router.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Components/dashboard/Home';
+
+const Router = () => {
+  return (
+    <div>
+      
+      <BrowserRouter>
+<Routes>
+<Route path="/" element={<Navbar/>}></Route>
+<Route path="home/" element={<Home/>}></Route>
+ </Routes>
+</BrowserRouter>
+    </div>
+  )
+}
+
+export default Router
