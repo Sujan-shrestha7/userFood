e1ed8808b68b2a952cd4954fa6bc28dbb13edf37
Sujanshrestha <sujanstha5732@gmail.com>
2025-02-07 11:37:07 +0545

    home

--- src/Components/dashboard/Home.tsx
+++ src/Components/dashboard/Home.tsx
@@ -3,6 +3,7 @@ import Navbar from "../Navbar";
 import homeImg from "../images/homeImg.png";
 import search from "../images/search.png";
 import download from "../images/download.png";
+import food from "../images/food.png";
 
 const Home = () => {
   return (
@@ -11,7 +12,7 @@ const Home = () => {
       <div className="flex flex-wrap items-center justify-center">
         <div className="mb-[60px]  ml-[110px]">
           <h1 className="question">Are You Hungry ?</h1>
-          <div className="w-[620px] h-[120px] flex flex-wrap gap-[20px] items-center justify-center mt-[30px] bg-[#fff] rounded-[10px]">
+          <div className="w-[620px] h-[120px] flex flex-wrap gap-[20px] items-center justify-center mt-[30px] bg-[#fff] rounded-[10px] shadow-lg">
             <div className="relative flex items-center justify-center">
               <input
                 type="text"
@@ -50,23 +51,59 @@ const Home = () => {
       </div>
       <div className="w-full h-[600px] bg-[#F9F9F9] relative">
         <div>
-        <p className="popularitem absolute top-[10%] left-[45%] text-center text-[28px]">
-          Popular Items
-        </p>
+          <p className="popularitem absolute top-[10%] left-[45%] text-center text-[28px]">
+            Popular Items
+          </p>
         </div>
-        <div className="pt-[200px] pl-[140px] flex flex-wrap gap-[20px]">
-            <div className="h-[250px] w-[280px] bg-[#000000] rounded-[30px]">
-
+        <div className="pt-[200px] pl-[160px] flex flex-wrap gap-[25px]">
+          <div className="h-[250px] w-[280px] bg-[#000000] rounded-[30px] relative border-none overflow-hidden">
+            <div className="absolute h-[100px] w-[100px] top-[160px] text-[#fff] bg-[#FCCD23] text-[25px] font-bold px-3 py-1 rounded-tr-[30px]">
+              <div className="flex mt-[15px] ml-[10px]">
+                <p className="text-[40px]">15</p>
+                <p>
+                  % <p className="text-[18px]">Off</p>
+                </p>
+              </div>
             </div>
-            <div className="h-[250px] w-[280px] bg-[#000000] rounded-[30px]">
+            <img src={food} className="h-full w-full object-cover" alt="Food" />
+          </div>
 
+          <div className="h-[250px] w-[280px] bg-[#000000] rounded-[30px] relative border-none overflow-hidden">
+            <div className="absolute h-[100px] w-[100px] top-[160px] text-[#fff] bg-[#FCCD23] text-[25px] font-bold px-3 py-1 rounded-tr-[30px]">
+              <div className="flex mt-[15px] ml-[10px]">
+                <p className="text-[40px]">15</p>
+                <p>
+                  % <p className="text-[18px]">Off</p>
+                </p>
+              </div>
             </div>
-            <div className="h-[250px] w-[280px] bg-[#000000] rounded-[30px]">
+            <img src={food} className="h-full w-full object-cover" alt="Food" />
+          </div>
 
+          <div className="h-[250px] w-[280px] bg-[#000000] rounded-[30px] relative border-none overflow-hidden">
+            <div className="absolute h-[100px] w-[100px] top-[160px] text-[#fff] bg-[#FCCD23] text-[25px] font-bold px-3 py-1 rounded-tr-[30px]">
+              <div className="flex mt-[15px] ml-[10px]">
+                <p className="text-[40px]">15</p>
+                <p>
+                  % <p className="text-[18px]">Off</p>
+                </p>
+              </div>
             </div>
-            <div className="h-[250px] w-[280px] bg-[#000000] rounded-[30px]">
+            <img src={food} className="h-full w-full object-cover" alt="Food" />
+          </div>
 
+          <div className="h-[250px] w-[280px] bg-[#000000] rounded-[30px] relative border-none overflow-hidden">
+            <div className="absolute h-[100px] w-[100px] top-[160px] text-[#fff] bg-[#FCCD23] text-[25px] font-bold px-3 py-1 rounded-tr-[30px]">
+              <div className="flex mt-[15px] ml-[10px]">
+                <p className="text-[40px]">15</p>
+                <p>
+                  % <p className="text-[18px]">Off</p>
+                </p>
+              </div>
             </div>
+            <img src={food} className="h-full w-full object-cover" alt="Food" />
+          </div>
+          
         </div>
       </div>
     </div>
