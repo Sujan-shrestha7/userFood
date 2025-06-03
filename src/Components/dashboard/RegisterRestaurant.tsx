77d3c3345130f3e1a3a85ae92b9d9f10969d7779
Sujanshrestha <sujanstha5732@gmail.com>
2025-06-03 16:59:52 +0545

    asf

--- src/Components/dashboard/RegisterRestaurant.tsx
+++ src/Components/dashboard/RegisterRestaurant.tsx
@@ -8,14 +8,24 @@ const RegisterRestaurant: React.FC = () => {
       <HomeNav />
       <div className="bg-[#fff] m-[35px] mx-[50px] rounded-[20px] flex">
         <div>
-        <img
-          src={woodart}
-          className="relative w-[825px] h-auto rounded-tl-[20px] rounded-bl-[20px] z-10 opacity-80"
-          alt="Wood Art"
-        />
+          <img
+            src={woodart}
+            className="relative w-[825px] h-auto rounded-tl-[20px] rounded-bl-[20px] z-10 opacity-80"
+            alt="Wood Art"
+          />
         </div>
-        <div className=" w-full my-[30px]">
-            <p className="text-center text-[24px] font-bold ">Registration Form</p>
+        <div className="w-full my-[30px]">
+          <p className="text-center text-[24px] font-bold">Registration Form</p>
+          <div className="m-[30px] mx-[30px]">
+            <div className="flex flex-col gap-y-[10px]">
+              <label className="font-bold text-[14px]">Full Name</label>
+              <input
+                type="text"
+                placeholder="Name"
+                className="w-full h-[35px] border-0 border-b-2 border-black outline-none py-2 px-1 placeholder-black-500 placeholder:text-[18px] placeholder:text-gray placeholder:font-bold"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
